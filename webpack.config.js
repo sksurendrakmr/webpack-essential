5c4dd539875621675868efb1e9ac97d108b0c613
sksurendrakmr <sksurendrakmr@gmail.com>
2022-10-02 23:31:46 +0530

    mini-css-extreact-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     module: {
         rules: [
             {
                 test: /\.css?$/,
-                use: ['style-loader','css-loader']
+                use: [MiniCssExtractPlugin.loader,'css-loader']
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin()]
 }
