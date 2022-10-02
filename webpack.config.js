104563384ca89812c04853a1fd15c8ff814c590a
sksurendrakmr <sksurendrakmr@gmail.com>
2022-10-03 00:33:46 +0530

    React app with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,20 @@ module.exports = {
             {
                 test: /\.ts$/,
                 use: ['ts-loader']
+            },
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
             }
         ]
+    },
+    resolve:{
+        extensions: ['.ts','.js','.jsx']
     },
     plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin()]
 }
